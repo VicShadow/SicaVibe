a9141d0abc628850ee25bb47fccffb9a75b55d26
marshaia <joana15501@gmail.com>
2023-06-14 15:03:57 +0100

    db pass

--- SicaVibeApp/scripts/DBUserCreate.sql
+++ SicaVibeApp/scripts/DBUserCreate.sql
@@ -1,3 +1,3 @@
-CREATE USER 'aa-sic-sicavibe'@'localhost' IDENTIFIED BY 'Sicavibe22-23';
+CREATE USER 'aa-sic-sicavibe'@'localhost' IDENTIFIED BY 'sicavibe22-23';
 GRANT ALL ON SicaVibe.* TO 'aa-sic-sicavibe'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
